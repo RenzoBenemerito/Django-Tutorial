daafb1f9b156af724c4e0090e1dfadb756684667
rbenemerito <rbenemerito1@gmail.com>
2019-06-06 14:46:57 +0800

    Added Publish, Security and Comment Functionalities

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
-
+admin.site.register(Comment)
 # my api token 2e1e89fcf80614441fd3600205eaf1a016132733
